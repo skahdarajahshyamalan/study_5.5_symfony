b2fc38af51f1b04baa776af0347500c0202ec825
RoyceRocithan <sivathanr5@gmail.com>
2023-12-26 19:01:16 +0530

    k

---
+++ src/Entity/Product.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * @ORM\Entity
+ */
+class Product
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="decimal", precision=10, scale=2)
+     */
+    private $price;
+
+    // ... other properties
+}
+?>
